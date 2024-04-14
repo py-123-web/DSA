aebb29a20474fd5afcd4504acf56e50b916fa078
pyhor <146723603+py-123-web@users.noreply.github.com>
2024-04-14 20:58:07 +0800

    Update CourseInitializer.java

--- dao/CourseInitializer.java
+++ dao/CourseInitializer.java
@@ -3,21 +3,38 @@ package dao;
 import adt.SortedArrayList;
 import adt.SortedListInterface;
 import entity.Course;
-//import java.util.Iterator;
+import entity.Programme;
+import java.util.Iterator;
 
 /**
- * Initializes the course list and map.
+ *
+ * @author Hor Pei Yu
  */
 public class CourseInitializer {
-    
-    // Using interfaces to allow flexibility in implementations
-    public static SortedListInterface<Course> courseInitializer() {
-        SortedListInterface<Course> courseList = new SortedArrayList<>(); // Initialize the list with Course type
-        
-        // Adding courses
-        courseList.add(new Course("BACS2003", "DSA", "L/T/P", "YEAR SEM 3",3, "FOCS"));
-        // Add other courses similarly...
-        
-        return courseList; // Return the initialized course list
+
+    private static SortedListInterface<Programme> progList = new SortedArrayList<>();
+    public static SortedListInterface<Course> courseList = new SortedArrayList<>();
+
+    public void CourseInfo() { // Specify type arguments
+        // Specify type arguments
+        courseList.add(new Course("BACS2003 (L)", "DATA STRUCTURE AND ALGORITHM", "Lecture", "YEAR 3 SEM 3", 3, "FOCS", 777.00, "REI"));
+        courseList.add(new Course("BAIT1093 (L)", "INTRODUCTION TO COMPUTER SECURITY", "Lecture", "YEAR 3 SEM 3", 3, "FOCS", 777.00, "REI"));
     }
+
+    public Iterator<Course> courseInit() {
+        return courseList.iterator();
+
+    }
+
+    public void ProgrammeInfo() { // Specify type arguments
+        // Specify type arguments
+        progList.add(new Programme("REI", "Bachelor of Information Systems (Honours) in Enterprise Information Systems "));
+
+        // Specify type arguments
+    }
+
+    public Iterator<Programme> programmeInit() {
+        return progList.iterator();
+    }
+
 }
